17ce37ef04b0b0741e5cfb0f61176992adec3296
Jimshi <Jimshi@10.84.142.119>
2018-03-07 17:29:09 -0500

    sd

--- Cuckerburg Project/src/code/GUI.java
+++ Cuckerburg Project/src/code/GUI.java
@@ -11,19 +11,22 @@ import javax.swing.*;
  * @author Benjamin Shapiro
  */
 
-public class GUI extends JFrame {
+public class GUI extends JFrame{
 
 	private static final long serialVersionUID = 1L;
 	private static final int WIDTH = 600;
 	private static final int HEIGHT = 400;
+	private JFrame frame;
+	private JPanel panel;
+	private JButton button;
+	
 
-	public static void main(String[] args) {
-
-		/**
-		 * Simple JFrame Need to Adjust later for devices
-		 */
-
-		JFrame frame = new JFrame("Cuckerburg (Name TBD)");
+	/**
+	 * Simple JFrame Need to Adjust later for devices
+	 */
+	
+	public void gui() {
+		frame = new JFrame("Cuckerburg");
 		frame.setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		frame.setMinimumSize(new Dimension(WIDTH, HEIGHT));
 		frame.setMaximumSize(new Dimension(WIDTH, HEIGHT));
@@ -31,8 +34,17 @@ public class GUI extends JFrame {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
-		frame.add(new Display());
-
+		
+		frame.add(new button("Cuck"));
 	}
 
+	public GUI() {
+		gui();
+	}
+	
+	public static void main(String[] args) {
+
+		new GUI();
+
+	}
 }
--- Cuckerburg Project/src/code/button.java
+++ Cuckerburg Project/src/code/button.java
@@ -1,53 +1,66 @@
 package code;
+
 import java.awt.*;
 import java.awt.event.*;
 
-public class button extends Frame implements WindowListener,ActionListener {
-        TextField text = new TextField(12);
-        TextField text2= new TextField(12);
-        Button b;
-        Button c;
-        private int numClicks = 0;
+public class button extends Frame implements WindowListener, ActionListener {
 
-        public static void main(String[] args) {
-                button myWindow = new button("My first window");
-                myWindow.setSize(200,100);
-                myWindow.setVisible(true);
-        }
+	private static final long serialVersionUID = 1L;
+	private int numClicks = 0;
+	
+	TextField text = new TextField(12);
+	TextField text2 = new TextField(12);
+	Button b;
+	Button c;
 
-        public button(String title) {
+	public static void main(String[] args) {
+		button myWindow = new button("Cucks");
+		myWindow.setSize(200, 100);
+		myWindow.setVisible(true);
+	}
 
-                super(title);
-                setLayout(new FlowLayout());
-                addWindowListener(this);
-                b = new Button("Votes");
-                c = new Button ("Votes");
-                add(c);
-                add(text2);
-                add(b);
-                add(text);
-                b.addActionListener(this);
-                c.addActionListener(this);
-        }
+	public button(String title) {
 
-        public void actionPerformed(ActionEvent e) {
-                numClicks++;
-                text2.setText("You got " + numClicks + " votes!");
-                text.setText("You got " + numClicks + " votes!");
-        }
+		super(title);
+		setLayout(new FlowLayout());
+		addWindowListener(this);
+		b = new Button("Votes");
+		c = new Button("Votes");
+		add(c);
+		add(text2);
+		add(b);
+		add(text);
+		b.addActionListener(this);
+		c.addActionListener(this);
+	}
 
-        public void windowClosing(WindowEvent e) {
-                dispose();
-                System.exit(0);
-        }
+	public void actionPerformed(ActionEvent e) {
+		numClicks++;
+		text2.setText("You got " + numClicks + " votes!");
+		text.setText("You got " + numClicks + " votes!");
+	}
 
-        public void windowOpened(WindowEvent e) {}
-        public void windowActivated(WindowEvent e) {}
-        public void windowIconified(WindowEvent e) {}
-        public void windowDeiconified(WindowEvent e) {}
-        public void windowDeactivated(WindowEvent e) {}
-        public void windowClosed(WindowEvent e) {}
+	public void windowClosing(WindowEvent e) {
+		dispose();
+		System.exit(0);
+	}
+
+	public void windowOpened(WindowEvent e) {
+	}
+
+	public void windowActivated(WindowEvent e) {
+	}
+
+	public void windowIconified(WindowEvent e) {
+	}
+
+	public void windowDeiconified(WindowEvent e) {
+	}
+
+	public void windowDeactivated(WindowEvent e) {
+	}
+
+	public void windowClosed(WindowEvent e) {
+	}
 
 }
-
-
