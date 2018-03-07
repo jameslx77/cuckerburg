450fb1cd2a6cd996fe356f13ec281e7b3b5950ea
Jimshi <Jimshi@10.84.142.119>
2018-03-07 03:21:13 -0500

    reading file method added

--- Cuckerburg Project/src/code/Game.java
+++ Cuckerburg Project/src/code/Game.java
@@ -1,6 +1,45 @@
 package code;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Game {
 
-	
+	private ArrayList<String> listOfPrompts;
+	private ArrayList<String> prompts;
+
+	public Game() {
+		this.listOfPrompts = new ArrayList<String>();
+		this.prompts = new ArrayList<String>();
+	}
+
+	/**
+	 * This method will add the prompts.txt to an ArrayList of strings.
+	 */
+
+	public void readTextFile() {
+		try {
+			String filename = "src/code/prompts.txt";
+			for (String line : Files.readAllLines(Paths.get(filename))) {
+				listOfPrompts.add(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Loops through the listOfPrompts ArrayList and picks out a random prompt to be
+	 * debated.
+	 */
+
+	public void getRandomPrompt() {
+		Collections.shuffle(listOfPrompts);
+		for (int i = 0; i < 2; i++) {
+			prompts.add(listOfPrompts.get(i));
+		}
+	}
 }
