3d9c1d12b9f833031b07db441a17c9ea9a87c4f7
Jimshi <Jimshi@10.84.142.119>
2018-03-07 02:48:11 -0500

    jframe updates

--- Cuckerburg Project/src/code/GUI.java
+++ Cuckerburg Project/src/code/GUI.java
@@ -1,5 +1,27 @@
 package code;
 
+import javax.swing.*;
+
+/**
+ * This is the GUI class
+ * @author James Li-Xu and Benjamin Shapiro
+ */
+
 public class GUI {
 
+	public static void main(String [] args) {
+	
+		/** Simple JFrame
+		 * Need to Adjust later for devices
+		 */
+		
+		JFrame frame = new JFrame("Cuckerburg (Name TBD)");
+		frame.setSize(1280, 1080);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(true);
+		frame.setLocationRelativeTo(null);
+		
+	}
+	
 }
